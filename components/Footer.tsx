c983cdc8ac16a6de97cd02edbd3e7406104a6e0d
Achchuthan2006 <kumarananthaachchuthan15@gmail.com>
2026-02-22 08:36:28 -0500

    Improve footer grid responsiveness across breakpoints

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -80,6 +80,9 @@ const footerGroups: FooterGroup[] = [
   },
 ]
 
+const primaryFooterGroups = footerGroups.slice(0, 5)
+const secondaryFooterGroups = footerGroups.slice(5)
+
 export default function Footer() {
   const year = new Date().getFullYear()
   const mapsQuery = encodeURIComponent(siteConfig.addressLines.join(", "))
@@ -182,11 +185,19 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="grid gap-8 sm:grid-cols-2 lg:col-span-8 lg:grid-cols-4">
-            {footerGroups.map((group) => (
-              <FooterColumn key={group.titleEn} group={group} />
-            ))}
-            <FooterInfo />
+          <div className="lg:col-span-8">
+            <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-5">
+              {primaryFooterGroups.map((group) => (
+                <FooterColumn key={group.titleEn} group={group} />
+              ))}
+            </div>
+
+            <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              {secondaryFooterGroups.map((group) => (
+                <FooterColumn key={group.titleEn} group={group} />
+              ))}
+              <FooterInfo />
+            </div>
           </div>
         </div>
 
