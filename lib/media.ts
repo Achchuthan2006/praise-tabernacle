6a54d8e49b39a8014761263d85849c6b66f2a99e
Achchuthan2006 <kumarananthaachchuthan15@gmail.com>
2026-02-22 09:02:09 -0500

    Use stable photo assets for homepage gallery

--- lib/media.ts
+++ lib/media.ts
@@ -8,35 +8,35 @@ export type MediaPhoto = {
 
 export const mediaPhotos: MediaPhoto[] = [
   {
-    src: "/photos/home/church-life.webp",
+    src: "/photos/home/church-life.jpeg",
     fallbackSrc: "/event-family.svg",
     alt: "Church life at Praise Tabernacle",
     label: "Church Life",
     href: "/events",
   },
   {
-    src: "/photos/home/adults-fellowship.webp",
+    src: "/photos/home/adults-fellowship.jpeg",
     fallbackSrc: "/event-teaching.svg",
     alt: "Adults fellowship at Praise Tabernacle",
     label: "Fellowship",
     href: "/groups",
   },
   {
-    src: "/photos/home/church-front.webp",
+    src: "/photos/home/church-front.jpg",
     fallbackSrc: "/event-holiday.svg",
     alt: "Praise Tabernacle church front",
     label: "Church Front",
     href: "/visit",
   },
   {
-    src: "/photos/home/church-front-2.webp",
+    src: "/photos/home/church-front-2.jpeg",
     fallbackSrc: "/event-community.svg",
     alt: "Praise Tabernacle church building",
     label: "Our Church",
     href: "/about",
   },
   {
-    src: "/photos/home/kids-ministry.webp",
+    src: "/photos/home/kids-ministry.jpeg",
     fallbackSrc: "/event-teaching.svg",
     alt: "Kids ministry at Praise Tabernacle",
     label: "Kids Ministry",
