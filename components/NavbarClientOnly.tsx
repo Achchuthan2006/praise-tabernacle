792a694daec95166c70eeb81ff3d7e12df09f78d
Achchuthan2006 <kumarananthaachchuthan15@gmail.com>
2026-02-23 00:28:11 -0500

    Prevent navbar hydration mismatch with client-mount render

--- components/NavbarClientOnly.tsx
+++ components/NavbarClientOnly.tsx
@@ -1,7 +1,19 @@
 "use client"
 
+import { useEffect, useState } from "react"
+
 import Navbar from "@/components/Navbar"
 
 export default function NavbarClientOnly() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return <div aria-hidden="true" className="h-16 xl:h-20" />
+  }
+
   return <Navbar />
 }
